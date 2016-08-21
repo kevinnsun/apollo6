2f9e77ce8b4d1d86c1d6c3bad3c7080e000225be
Richard <richardzy.chen@mail.utoronto.ca>
2016-08-21 10:23:10 -0400

    updates index and logo on all pages

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     setTimeout(function() {
-        $(".logo").transition({ opacity: 1 }, 4000);
+        $(".cover").transition({ opacity: 1 }, 4000);
     }, 1000);
     setTimeout(function() {
-        $(".button").transition({ opacity: 1 }, 4000);
+        $(".button").transition({ opacity: 1 }, 2000);
     }, 3000);
 
 })
